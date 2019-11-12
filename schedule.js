d2097a30eee0b2b97a214f32fc8d28df8a054c48
jinling <luoage@msn.cn>
2019-11-12 17:08:42 +0800

    args

--- schedule.js
+++ schedule.js
@@ -23,7 +23,7 @@ module.exports = function (expression, cb, options) {
   options = options || {};
 
   const date = options.date;
-  const args = options.args || {};
+  const args = options.args;
   const errorFn = options.errorFn || console.error;
 
   const exOpts = { currentDate: new Date() };
@@ -34,7 +34,7 @@ module.exports = function (expression, cb, options) {
   if (isExec && typeof cb === 'function') {
     eventEmitter.emit('crontab-node start');
     Promise.resolve().then(() => {
-        return cb(...args);
+        return cb.apply(this, [].concat(args));
       }).then(() => {
         eventEmitter.emit('crontab-node end');
       }).catch((err) => {
